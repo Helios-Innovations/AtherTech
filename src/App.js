be8a1edc415240a90fcb57fefb81cadd6af67513
Madhav060 <128737362+Madhav060@users.noreply.github.com>
2024-08-10 21:09:47 +0530

    Pushing the website code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './Components/Navbar';
+import Service from './Components/Services/Service';
+import Home from './Components/Landing/home';
+import { createBrowserRouter, RouterProvider, BrowserRouter,Route,Routes } from 'react-router-dom';
+import Footer from './footer/Footer';
+import Contact from './Components/CONT/Contact';
+import AboutUs from './Components/ABOUT/About';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+       <Navbar/>
+
+       <Routes>
+       <Route path="/" element={<Home/>}/>
+       <Route path="/services" element={<Service/>}/>
+       <Route path="/about" element={<AboutUs/>}/>
+       <Route path="/contact" element={<Contact/>}/>
+       </Routes>
+       <Footer/>
+       </BrowserRouter>
     </div>
   );
 }
